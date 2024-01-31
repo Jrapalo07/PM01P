3600ca11976193fedc90c69fea8ed3e27d0ac213
Jrapalo07 <138176649+Jrapalo07@users.noreply.github.com>
2024-01-31 09:22:31 -0600

    Visualizacion de datos en lista 30/01/2024

--- app/src/main/java/com/example/pm01p/MainActivity.java
+++ app/src/main/java/com/example/pm01p/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.pm01p;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import Configuracion.SQLiteConexion;
+import Configuracion.Transacciones;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText nombres, apellidos, telefono;
+    EditText nombres, apellidos, edad, correo;
     Button btnproceso;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
         nombres= (EditText) findViewById(R.id.nombres);
         apellidos= (EditText) findViewById(R.id.apellidos);
-        telefono= (EditText) findViewById(R.id.telefono);
+        edad= (EditText) findViewById(R.id.edad);
+        correo= (EditText) findViewById(R.id.correo);
         btnproceso= (Button) findViewById(R.id.btn_procesar);
 
         btnproceso.setOnClickListener(new View.OnClickListener() {
@@ -30,11 +37,20 @@ public class MainActivity extends AppCompatActivity {
 
     private void AddPerson()
     {
-        SQLiteConexion conexion = SQLiteConexion( this, Transacciones.DBName, null, 1);
-        SQLiteDatabase db = conexion.getReadableDatabase();
+        SQLiteConexion conexion = new SQLiteConexion( this, Transacciones.DBName, null, 1);
+        SQLiteDatabase db = conexion.getWritableDatabase();
 
         ContentValues valores = new ContentValues();
         valores.put(Transacciones.nombres, nombres.getText().toString());
+        valores.put(Transacciones.apellidos, apellidos.getText().toString());
+        valores.put(Transacciones.edad, edad.getText().toString());
+        valores.put(Transacciones.correo, correo.getText().toString());
+
+        Long resultado = db.insert(Transacciones.TablePersonas, Transacciones.id, valores);
+
+        Toast.makeText(getApplicationContext(), "Registro ingresado con exito " + resultado.toString(),
+                    Toast.LENGTH_LONG).show();
+        db.close();
     }
 
 
